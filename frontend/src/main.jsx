a7166e9290e87b4797b9664446e261833e527864
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-06-14 22:46:52 +0530

    error-in-frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
+// main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+export const queryClient = new QueryClient();
 
-const queryClient = new QueryClient()
-createRoot(document.getElementById('root')).render(
-<QueryClientProvider client={queryClient}>
-  <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
   </QueryClientProvider>
-
-)
+);
