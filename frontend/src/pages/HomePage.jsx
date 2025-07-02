a578e10c879a80a7f90f917946e3b6828ae18168
Ayushpaney24 <ayushpandey4480@gmail.com>
2025-07-02 19:46:25 +0530

    forntend-re-set

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
+
 import React from 'react'
-import Urlform from '../components/Urlform'
+import UrlForm from '../components/UrlFrom'
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h1 className='text-2xl font-bold text-center mb-6'> URL Shortener</h1>
-        <Urlform />
-      </div>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+    <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+      <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
+      <UrlForm/>
     </div>
+  </div>
   )
 }
 
